e78e924bd45652dc701dca907bdfd7aad5e74509
Ritik Chawla <ritikchawla2014@gmail.com>
2023-08-14 21:42:25 +0530

    Basic manual Load balancer

---
+++ src/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"sync"
+)
+
+type Server interface {
+	Address() string
+	IsAlive() bool
+	Serve(rw http.ResponseWriter, r *http.Request)
+}
+
+type simpleServer struct {
+	addr  string
+	proxy *httputil.ReverseProxy
+}
+
+func newSimpleServer(addr string) *simpleServer {
+	serverUrl, err := url.Parse(addr)
+	handleErr(err)
+
+	return &simpleServer{
+		addr:  addr,
+		proxy: httputil.NewSingleHostReverseProxy(serverUrl),
+	}
+}
+
+type LoadBalancer struct {
+	port            string
+	roundRobinCount int
+	servers         []Server
+	mutex           sync.Mutex
+}
+
+func NewLoadBalancer(port string, servers []Server) *LoadBalancer {
+	return &LoadBalancer{
+		port:            port,
+		roundRobinCount: 0,
+		servers:         servers,
+	}
+}
+
+func handleErr(err error) {
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func (s *simpleServer) Address() string { return s.addr }
+
+func (s *simpleServer) IsAlive() bool { return true }
+
+func (s *simpleServer) Serve(rw http.ResponseWriter, req *http.Request) {
+	s.proxy.ServeHTTP(rw, req)
+}
+
+func (lb *LoadBalancer) getNextAvailableServer() Server {
+	lb.mutex.Lock()
+	defer lb.mutex.Unlock()
+
+	startIndex := lb.roundRobinCount
+	for i := 0; i < len(lb.servers); i++ {
+		serverIndex := (startIndex + i) % len(lb.servers)
+		if lb.servers[serverIndex].IsAlive() {
+			lb.roundRobinCount = (serverIndex + 1) % len(lb.servers)
+			return lb.servers[serverIndex]
+		}
+	}
+	lb.roundRobinCount = 0
+	return nil
+}
+
+func (lb *LoadBalancer) serveProxy(rw http.ResponseWriter, req *http.Request) {
+	targetServer := lb.getNextAvailableServer()
+	if targetServer != nil {
+		fmt.Printf("Forwarding request to address %q\n", targetServer.Address())
+		targetServer.Serve(rw, req)
+	} else {
+		http.Error(rw, "No available server", http.StatusServiceUnavailable)
+	}
+}
+
+func main() {
+	servers := []Server{
+		newSimpleServer("https://www.google.com/"),
+		newSimpleServer("https://www.duckduckgo.com"),
+		newSimpleServer("https://www.bing.com/"),
+	}
+	lb := NewLoadBalancer("8000", servers)
+	handleRedirect := func(rw http.ResponseWriter, req *http.Request) {
+		lb.serveProxy(rw, req)
+	}
+	http.HandleFunc("/", handleRedirect)
+	fmt.Printf("Server serving at 'localhost:%s'\n", lb.port)
+	http.ListenAndServe(":"+lb.port, nil)
+}
